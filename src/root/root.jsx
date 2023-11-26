9b168669feb484bbb8ed6bb9471d1506ce961d9f
PalmisanoGaston <130399416+PalmisanoGaston@users.noreply.github.com>
2023-11-26 11:25:58 -0300

    Update root.jsx

--- src/root/root.jsx
+++ src/root/root.jsx
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import './rootstyle.css';
 import imagen from './13007.png';
 import imagen2 from './13009.png';
 
 export default function Root() {
   const [comentarios, setComentarios] = useState([]);
-  const [go, setGo] = useState(null);
   const [admin, setAdmin] = useState(false);
   const [showButton, setShowButton] = useState(false);
+  const navigate = useNavigate(); // Agrega useNavigate
+
   useEffect(() => {
     const DownAdmin = localStorage.getItem("admin");
     setAdmin(JSON.parse(DownAdmin));
-
-    if (admin) {
-      setShowButton(admin);
-    } else {
-      setShowButton(admin);
-    }
-
-   
+    setShowButton(admin);
   }, [admin]);
 
   useEffect(() => {
@@ -30,11 +25,14 @@ export default function Root() {
     }
   }, []);
 
-   const HandleClick = (id) => {
-  
+  const HandleClick = (id) => {
     const updatedComentarios = comentarios.filter((comentario) => comentario.id !== id);
     setComentarios(updatedComentarios);
     localStorage.setItem("comentarios", JSON.stringify(updatedComentarios));
+  };
+
+  const handleVerMasClick = (id) => {
+    navigate(`/coment/${parseInt(id)}`); // Navega a la página específica
   };
 
   return (
@@ -48,22 +46,28 @@ export default function Root() {
       <div className="sidebar">
         <div>
           <form method="post"></form>
-           </div>
+        </div>
         <nav></nav>
-      
+
         <div className="EsteDiv">
           <ul>
             {comentarios.map((comentario, index) => (
               <div key={index} className="Post">
                 <h2 className="">{comentario.nombre}</h2>
                 <h1 className="TituloPost">{comentario.Titulo}</h1>
+                {comentario.archivo && (
+                  <img
+                    src={comentario.archivo.dataURL}  // asumimos que el campo archivo es un objeto con la propiedad dataURL
+                    alt={comentario.archivo.name}
+                    style={{ maxWidth: '100px', maxHeight: '100px' }}
+                  />
+                )}
                 <p>{comentario.hora}:{comentario.minutos}:{comentario.segundos}</p>
-                <button className="Boton">
-                  <a className="linkardo" href={`/coment/${parseInt(comentario.id)}`}>VER MAS</a>
-                </button>
+                <button className="Boton" onClick={() => handleVerMasClick(comentario.id)}>VER MAS</button>
                 {showButton && (
                   <button className="Borrar" onClick={() => HandleClick(comentario.id)}>BORRAR</button>
                 )}
+               
               </div>
             ))}
           </ul>
